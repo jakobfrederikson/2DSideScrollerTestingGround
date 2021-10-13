c461f9135ac4217ef93cb0e42e442ff3e15028ea
Jakob <jakobfred1999@gmail.com>
2021-10-13 22:07:29 +1300

    added power up rotation

--- Assets/Scripts/DoubleJumpPowerUp.cs
+++ Assets/Scripts/DoubleJumpPowerUp.cs
@@ -4,6 +4,33 @@ using UnityEngine;
 
 public class DoubleJumpPowerUp : MonoBehaviour
 {
+    public float degreesPerSecond = 15.0f;
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    // Use this for initialization
+    void Start()
+    {
+        // Store the starting position & rotation of the object
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Spin object around Y-Axis
+        transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
+
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
